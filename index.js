27cc1811066e27a88b5d5652b3af0979e9efefbb
davidbrudascu <d.brudascu@gmail.com>
2021-12-06 19:43:14 +0200

    lalal

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+      headless: true,
+      args:  ['--enable-features=NetworkService'],
+      ignoreHTTPSErrors: true
+  });
+  const page = await browser.newPage();
+  await page.goto('https://caltrackr.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
